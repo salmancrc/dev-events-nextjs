8c9a9afd48d6e78b0b6876e40287f1fe005d8fca
salman <saahmad@mtm-inc.net>
2026-01-16 22:13:29 +0500

    Event List fetching and displays it

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import {IEvent} from "@/database";
-import {cacheLife} from "next/cache";
+import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -11,26 +11,26 @@ const Page = async () => {
     const response = await fetch(`${BASE_URL}/api/events`);
     const { events } = await response.json();
 
-    return (
-        <section>
-            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
-            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+	return (
+		<section>
+			<h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+			<p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
-            <ExploreBtn />
+			<ExploreBtn />
 
-            <div className="mt-20 space-y-7">
-                <h3>Featured Events</h3>
+			<div className="mt-20 space-y-7">
+				<h3>Featured Events</h3>
 
-                <ul className="events">
-                    {events && events.length > 0 && events.map((event: IEvent) => (
-                        <li key={event.title} className="list-none">
-                            <EventCard {...event} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    )
+				<ul className="events">
+					{events && events.length > 0 && events.map((event: IEvent) => (
+						<li key={event.title} className="list-none">
+							<EventCard {...event} />
+						</li>
+					))}
+				</ul>
+			</div>
+		</section>
+	)
 }
 
 export default Page;
