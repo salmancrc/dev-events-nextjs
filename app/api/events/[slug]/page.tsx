49778a920986614cc4481dd518d63202878c4352
salman <saahmad@mtm-inc.net>
2026-01-20 22:52:42 +0500

    event details page added

---
+++ app/api/events/[slug]/page.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
+const EventDetailItem = ({ icon, alt, label }: { icon: string, alt: string, label: string }) =>
+(
+  <div className="flex-row-gap-2 item-center">
+    <Image src={icon} alt={alt} width={17} height={17} />
+    <p>{label}</p>
+  </div>
+)
+
+const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
+  <div className="agenda">
+    <h2>Agenda</h2>
+    <ul>
+      {agendaItems.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  </div>
+)
+
+const EventTags = ({ tags }: { tags: string[] }) => (
+  <div className="flex flex-row gap-1.5 flex-wrap">
+    {tags.map((tag) => (
+      <div className="pill" key={tag}>
+        {tag}
+      </div>
+    ))}
+  </div>
+)
+
+const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params;
+  const response = await fetch(`${BASE_URL}/api/events/${slug}`);
+  const { event: { description, image, overview, date, time, location, mode, agenda, audience, tags, organizer } } = await response.json();
+
+  if (!description) return notFound();
+
+  return (
+    <section id="event">
+      <div className="header">
+        <h1>Event Description</h1>
+        <p className="mt-2">{description}</p>
+      </div>
+
+      <div className="details">
+        <div className="content">
+          <Image src={image} alt="Event Banner" width={800} height={800} className="banner" />
+
+          <section className="flex-col-gap-2">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+          </section>
+
+          
+
+          <EventTags tags={JSON.parse(tags[0])}/>
+        </div>
+        <aside className="booking">
+          <p className="text-lg font-semibold">Book Event</p>
+        </aside>
+      </div>
+    </section>
+  )
+}
+
+export default EventDetailsPage
